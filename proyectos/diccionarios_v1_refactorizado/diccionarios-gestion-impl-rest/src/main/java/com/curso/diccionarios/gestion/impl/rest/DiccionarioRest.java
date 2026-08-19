1e9b1475c62d0f169a2d4fa77480fc2545e0dd1f
Iván Osuna Ayuste <ivan@iMac-Pro-de-Ivan.local>
2026-08-19 14:59:35 +0200

    dia3

--- proyectos/diccionarios_v1_refactorizado/diccionarios-gestion-impl-rest/src/main/java/com/curso/diccionarios/gestion/impl/rest/DiccionarioRest.java
+++ proyectos/diccionarios_v1_refactorizado/diccionarios-gestion-impl-rest/src/main/java/com/curso/diccionarios/gestion/impl/rest/DiccionarioRest.java
@@ -3,22 +3,36 @@ package com.curso.diccionarios.gestion.impl.rest;
 import java.util.List;
 import java.util.Optional;
 
+import com.curso.diccionarios.gestion.respuesta.palabra.PalabraEncontrada;
 import com.curso.diccionarios.gestion.respuesta.palabra.RespuestaPalabra;
 import com.curso.diccionarios.gestion.Diccionario;
 
 public class DiccionarioRest implements Diccionario {
 
-    public RespuestaPalabra dameSignificados(String palabra) {
-        return null;
+    private String rutaServidor;
+
+    public DiccionarioRest(String rutaServidor) {
+        this.rutaServidor = rutaServidor;
     }
 
     public boolean existe(String palabra){
-        return false;
-
+        return switch(dameSignificados(palabra)) {
+            case PalabraEncontrada palabraEncontrada -> true;
+            default                                  -> false;
+        };
     }
 
     public Optional<List<String>> getSignificados(String palabra) {
-            return Optional.empty();
-
+        return switch(dameSignificados(palabra)) {
+            case PalabraEncontrada palabraEncontrada -> Optional.of(palabraEncontrada.significados());
+            default                                  -> Optional.empty();
+        };
     }
+
+
+    public RespuestaPalabra dameSignificados(String palabra) {
+        return null;
+        // TODO
+    }
+
 }
--- proyectos/diccionarios_v1_refactorizado/diccionarios-gestion-impl-rest/src/main/java/com/curso/diccionarios/gestion/impl/rest/SuministradorDeDiccionariosRest.java
+++ proyectos/diccionarios_v1_refactorizado/diccionarios-gestion-impl-rest/src/main/java/com/curso/diccionarios/gestion/impl/rest/SuministradorDeDiccionariosRest.java
@@ -1,9 +1,15 @@
 package com.curso.diccionarios.gestion.impl.rest;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.URI;
 import java.util.Optional;
 
+import com.curso.diccionarios.gestion.respuesta.diccionario.DiccionarioEncontrado;
+import com.curso.diccionarios.gestion.respuesta.diccionario.DiccionarioNoEncontrado;
+import com.curso.diccionarios.gestion.respuesta.diccionario.ErrorAlObtenerDiccionario;
 import com.curso.diccionarios.gestion.respuesta.diccionario.RespuestaDiccionario;
 import com.curso.diccionarios.gestion.SuministradorDeDiccionarios;
-import com.curso.diccionarios.gestion.impl.rest.DiccionarioRest;
 import com.curso.diccionarios.gestion.Diccionario;
 public class SuministradorDeDiccionariosRest implements SuministradorDeDiccionarios {
 
@@ -14,16 +20,46 @@ public class SuministradorDeDiccionariosRest implements SuministradorDeDiccionar
     }
 
     public boolean tienesDiccionarioDe(String idioma){
-        return false;
+        return switch(getDiccionario(idioma)) {
+            case DiccionarioEncontrado encontrado -> true;
+            default                               -> false;
+        };
 
     }
 
     public Optional<Diccionario> dameDiccionario(String idioma){
-        return Optional.empty();
+        return switch(getDiccionario(idioma)) {
+            case DiccionarioEncontrado encontrado -> Optional.of(new DiccionarioRest( rutaServidor));
+            default                               -> Optional.empty();
+        };
     }
 
     public RespuestaDiccionario getDiccionario(String idioma) {
-        return null;
+        // Aqui va la llamada al servidor.
+        // A la ruta /v1/diccionarios/{idioma}
+        // Necesitamos mirar el código de estado:
+        // - 200 -> DiccionarioEncontrado
+        // - 404 -> DiccionarioNoEncontrado
+        // - 500 -> ErrorAlObtenerDiccionario
+        // Vamos a hacer la petición con el aAPI nueva de Java 11, HttpClient / HttpRequest 
+        // Manejados con patrones builder y switch expressions.
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(rutaServidor + "/v1/diccionario/" + idioma))
+                .HEAD()
+                .build();
+        // Lanzo el request usando mi cliente y obtengo la respuesta
+        try {
+            var response = client.send(request, BodyHandlers.discarding());
+            int statusCode = response.statusCode();
+            return switch (statusCode) {
+                case 200 -> new DiccionarioEncontrado( new DiccionarioRest( rutaServidor));
+                case 404 -> new DiccionarioNoEncontrado(idioma);
+                default -> new ErrorAlObtenerDiccionario("Error interno del servidor");
+            };
+        } catch (Exception e) {
+            return new ErrorAlObtenerDiccionario(e.getMessage());
+        }
     }
 
 }
